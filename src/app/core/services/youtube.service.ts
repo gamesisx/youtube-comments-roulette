90b58ba773593696acacc12ed6dfc6b10e11faab
Maximino Castillo <gamesis.pkxd.contacto@gmail.com>
2024-12-04 16:35:40 -0600

    Add a complete roulette for pick a comment

---
+++ src/app/core/services/youtube.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class YoutubeService {
+  private apiUrl = 'https://www.googleapis.com/youtube/v3';
+  private apiKey: string = environment.apiKey;
+
+  constructor(private http: HttpClient) {}
+
+  getComments(videoId: string, pageToken: string = ''): Observable<any> {
+    const url = `${this.apiUrl}/commentThreads?part=snippet,replies&videoId=${videoId}&key=${this.apiKey}&pageToken=${pageToken}&maxResults=100`;
+    return this.http.get<any>(url);
+  }
+}
